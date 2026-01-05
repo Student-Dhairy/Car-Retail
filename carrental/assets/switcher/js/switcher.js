500d72b05023c63e1e493a8619b92b7fe5dd938e
Student-Dhairy <dhairyaghera402@gmail.com>
2026-01-05 10:36:57 +0530

    Add cookie management functions and demo switcher

--- carrental/assets/switcher/js/switcher.js
+++ carrental/assets/switcher/js/switcher.js
@@ -43,3 +43,29 @@ function createCookie(name,value,days){
 	else var expires = "";
 	document.cookie = name+"="+value+expires+"; path=/";
 }
+function readCookie(name){
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++){
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+function eraseCookie(name){
+	createCookie(name,"",-1);
+}
+
+// DEMO Swticher Base
+jQuery('.demo_changer .demo-icon').click(function(){
+	if(jQuery('.demo_changer').hasClass("active")){
+		jQuery('.demo_changer').animate({"left":"-71px"},function(){
+			jQuery('.demo_changer').toggleClass("active");
+		});						
+	}else{
+		jQuery('.demo_changer').animate({"left":"0px"},function(){
+			jQuery('.demo_changer').toggleClass("active");
+		});			
+	} 
+});
